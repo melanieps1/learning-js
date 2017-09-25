e159e2995bbfc985c71dffcd3e399efc4a0e7fc6
Melanie Stoeckle <mpstoeckle@gmail.com>
2017-09-25 10:25:49 -0400

    initial commit, three ways to add javascript

---
+++ app/js/main.js
@@ -0,0 +1,6 @@
+function warning() {
+	// The console.log statement below logs the message to Chrome > Inspect > Console
+	// Console.log statements are used mostly for debugging
+	console.log('warning triggered');
+	alert("Danger, Danger!");
+}
